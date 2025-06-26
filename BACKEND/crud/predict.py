f8ba6c0bd53128ad403f1c8a21a97f6c9f024ded
Veerman14 <hasibuanfirman1404@gmail.com>
2025-06-26 18:00:39 +0700

    feat: implement crud operation

---
+++ BACKEND/crud/predict.py
@@ -0,0 +1,12 @@
+from models.predict import PredictionHistory
+from sqlalchemy.orm import Session
+
+def create_prediction(db: Session, prediction: dict):
+    db_entry = PredictionHistory(**prediction)
+    db.add(db_entry)
+    db.commit()
+    db.refresh(db_entry)
+    return db_entry
+
+def get_predictions(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(PredictionHistory).offset(skip).limit(limit).all()
