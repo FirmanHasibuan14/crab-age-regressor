321d874ded0977b904f4df97d626238859d40509
Veerman14 <hasibuanfirman1404@gmail.com>
2025-06-26 18:10:24 +0700

    final initial commit

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from routers import predict
+from core.config import Settings
+from database.connection import create_tables
 
-app = FastAPI()
+create_tables
+
+app = FastAPI(
+    title = Settings.APP_NAME
+)
+
+app.include_router(predict.router, prefix="/predict", tags=['Predict'])
 
 @app.get("/", tags=["Root"])
 async def root():
